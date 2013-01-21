af520d9ec7f463e7a7a2c1f6f52df6d95f34f94d
Ethan Jucovy <ejucovy@gmail.com>
2013-01-21 14:24:16 -0500

    allow configuration of alternate font for body text

--- lightertheme/__init__.py
+++ lightertheme/__init__.py
@@ -5,23 +5,43 @@ from genshi.filters import Transformer
 from genshi.filters.transform import StreamBuffer
 
 from trac.core import *
+from trac.config import *
 from trac.web.chrome import Chrome, add_stylesheet, add_script
 from trac.web.chrome import ITemplateProvider
-from trac.web.api import IRequestFilter, ITemplateStreamFilter
+from trac.web.api import IRequestHandler, ITemplateStreamFilter
 
 class Theme(Component):
-    implements(ITemplateStreamFilter, ITemplateProvider)
+    implements(ITemplateStreamFilter, ITemplateProvider, IRequestHandler)
 
+    extra_stylesheets = ListOption(
+        'lightertheme', 'extra_stylesheets',
+        'http://fonts.googleapis.com/css?family=Ubuntu')
+    main_text_font_family = Option(
+        'lightertheme', 'main_text_font_family', 'Ubuntu')
+        
+    def match_request(self, req):
+        if req.path_info == "/lightertheme/theme.css":
+            return True
+
+    def process_request(self, req):
+        data = {
+            'main_text_font_family': self.main_text_font_family,
+            }
+        output = Chrome(self.env).render_template(req,
+            "lightertheme/theme.css.tmpl", data, "text/plain")
+        req.send(output, 'text/css')
+    
     def filter_stream(self, req, method, filename, stream, data):
         """
         Wrap the banner and mainnav in a single banner_wrapper div
         """
 
-        add_stylesheet(req, 'http://fonts.googleapis.com/css?family=Ubuntu')
-        add_stylesheet(req, 'lightertheme/theme.css')
+        for href in self.extra_stylesheets:
+            add_stylesheet(req, href)
+        add_stylesheet(req, '/lightertheme/theme.css')
 
         stream |= Transformer("//div[@id='banner']").wrap(tag.div(class_="banner_wrapper banner_wrapper_first"))
-        stream |= Transformer("//div[@id='mainnav']").wrap(tag.div(class_="banner_wrapper banner_wrapper_second"))
+        stream |= Transformer("//div[@id='mainnav']").wrap(tag.div(class_="banner_wra7pper banner_wrapper_second"))
         stream |= Transformer("//div[@class='banner_wrapper banner_wrapper_first']").append(tag.hr())
         return stream
 
@@ -44,12 +64,12 @@ class Theme(Component):
         The `abspath` is the absolute path to the directory containing the
         resources on the local file system.
         """
-        yield "lightertheme", resource_filename(__name__, 'htdocs')
+        return []
 
     def get_templates_dirs(self):
         """Return a list of directories containing the provided template
         files.
         """
-        return []
+        return ["lightertheme", resource_filename(__name__, 'templates')]
 
 
